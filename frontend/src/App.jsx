1c1a69a32baab6211a2cbf6993ee73a8584b4c50
Nguyen Hoang Nhat Quang <112571466+nhnquang11@users.noreply.github.com>
2024-05-17 10:23:01 +0700

    Init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">
+        Dots
+      </h1>
+      <p>A place where the mind dances.</p>
+    </>
+  )
+}
+
+export default App
