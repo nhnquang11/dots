f66463c77b38d1f12d95310c3378029185d53665
Nguyen Hoang Nhat Quang <112571466+nhnquang11@users.noreply.github.com>
2024-05-18 01:23:01 +0700

    add dashboard tab

--- frontend/src/components/Stats.jsx
+++ frontend/src/components/Stats.jsx
@@ -1,8 +1,8 @@
 const Stats = () => {
   return (
-    <div className="mb-20 mx-10">
-      <h3 className="mt-12 font-serif text-4xl">Currently there are...</h3>
-      <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 rounded-lg gap-3">
+    <div className="mb-20 mx-3 sm:mx-10">
+      <h3 className="mt-16 font-serif text-4xl">Currently there are...</h3>
+      <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 rounded-lg gap-3">
         <div className="px-6 py-6 border rounded shadow-sm hover:shadow-lg cursor-pointer">
           <dt className="font-serif text-sm text-neutral-500">Visitors</dt>
           <dd className="font-serif text-3xl mt-1">30034</dd>
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,6 +3,15 @@ import Stats from "../components/Stats"
 const Dashboard = () => {
   return (
     <div>
+      <div className="mt-16 flex justify-center">
+        <div className="max-[450px]:max-w-[280px] flex justify-center flex-wrap gap-3 text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+          <button className="leading-8 font-extralight text-sm font-serif px-3 rounded bg-neutral-900 text-white hover:bg-neutral-600 transition duration-200 ease-in-out">Overview</button>
+          <button className="leading-8 font-extralight text-sm font-serif px-3 rounded text-neutral-900 border border-neutral-900 hover:bg-neutral-200 transition duration-200 ease-in-out">Profile</button>
+          <button className="leading-8 font-extralight text-sm font-serif px-3 rounded text-neutral-900 border border-neutral-900 hover:bg-neutral-200 transition duration-200 ease-in-out">Comments</button>
+          <button className="leading-8 font-extralight text-sm font-serif px-3 rounded text-neutral-900 border border-neutral-900 hover:bg-neutral-200 transition duration-200 ease-in-out">Users</button>
+          <button className="leading-8 font-extralight text-sm font-serif px-3 rounded text-neutral-900 border border-neutral-900 hover:bg-neutral-200 transition duration-200 ease-in-out">Posts</button>
+        </div>
+      </div>
       <Stats />
     </div>
   )
