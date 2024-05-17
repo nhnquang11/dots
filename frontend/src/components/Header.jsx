12f203b31a50bd013b96f3f093a3d03dc858f5c5
Nguyen Hoang Nhat Quang <112571466+nhnquang11@users.noreply.github.com>
2024-05-17 13:16:52 +0700

    remove zoom in when focus text input on safari

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         {/* Profile */}
         <div className="flex justify-center align-center">
           <button type="button">
-            <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
+            <img className="h-8 w-8 rounded-full object-cover" src="https://huggingface.co/KappaNeuro/director-darren-aronofsky-style/resolve/main/2345259.jpeg" />
           </button>
         </div>
       </div>
