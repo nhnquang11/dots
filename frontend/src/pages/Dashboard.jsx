8cfb4faae324e0ee968b37afe0ff989b2a1be26a
Nguyen Hoang Nhat Quang <112571466+nhnquang11@users.noreply.github.com>
2024-05-18 05:16:28 +0700

    add focus effect to dashboard tab items

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,11 +5,16 @@ import Comments from "../components/Comments"
 import Users from "../components/Users"
 import Stories from "../components/Stories"
 
+const active = "leading-8 font-extralight text-sm font-serif px-3 rounded bg-neutral-900 text-white hover:bg-neutral-600 transition duration-100 ease-in-out"
+const inActive = "leading-8 font-extralight text-sm font-serif px-3 rounded text-neutral-900 border border-neutral-900 hover:bg-neutral-200 transition duration-100 ease-in-out"
+
 const Dashboard = () => {
   const [componentToShow, setComponentToShow] = useState(<Overview />)
+  const [componentName, setComponentName] = useState('overview')
 
   const handleButtonOnClick = (e) => {
     const component = e.target.value
+    setComponentName(component)
     if (component === 'overview') setComponentToShow(<Overview />);
     else if (component === 'profile') setComponentToShow(<Profile />);
     else if (component === 'comments') setComponentToShow(<Comments />);
@@ -21,11 +26,11 @@ const Dashboard = () => {
     <div>
       <div className="mt-16 flex justify-center">
         <div className="max-[450px]:max-w-[280px] flex justify-center flex-wrap gap-3 text-sm font-medium text-center text-gray-500 dark:text-gray-400">
-          <button onClick={handleButtonOnClick} value="overview" className="leading-8 font-extralight text-sm font-serif px-3 rounded bg-neutral-900 text-white hover:bg-neutral-600 transition duration-200 ease-in-out">Overview</button>
-          <button onClick={handleButtonOnClick} value="profile" className="leading-8 font-extralight text-sm font-serif px-3 rounded text-neutral-900 border border-neutral-900 hover:bg-neutral-200 transition duration-200 ease-in-out">Profile</button>
-          <button onClick={handleButtonOnClick} value="comments" className="leading-8 font-extralight text-sm font-serif px-3 rounded text-neutral-900 border border-neutral-900 hover:bg-neutral-200 transition duration-200 ease-in-out">Comments</button>
-          <button onClick={handleButtonOnClick} value="users" className="leading-8 font-extralight text-sm font-serif px-3 rounded text-neutral-900 border border-neutral-900 hover:bg-neutral-200 transition duration-200 ease-in-out">Users</button>
-          <button onClick={handleButtonOnClick} value="stories" className="leading-8 font-extralight text-sm font-serif px-3 rounded text-neutral-900 border border-neutral-900 hover:bg-neutral-200 transition duration-200 ease-in-out">Stories</button>
+          <button onClick={handleButtonOnClick} value="overview" className={componentName === "overview" ? active : inActive}>Overview</button>
+          <button onClick={handleButtonOnClick} value="profile" className={componentName === "profile" ? active : inActive}>Profile</button>
+          <button onClick={handleButtonOnClick} value="comments" className={componentName === "comments" ? active : inActive}>Comments</button>
+          <button onClick={handleButtonOnClick} value="users" className={componentName === "users" ? active : inActive}>Users</button>
+          <button onClick={handleButtonOnClick} value="stories" className={componentName === "stories" ? active : inActive}>Stories</button>
         </div>
       </div>
       {componentToShow}
