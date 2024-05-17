fb86e7eab4929836fe857f5578e45dd74df97d1a
Nguyen Hoang Nhat Quang <112571466+nhnquang11@users.noreply.github.com>
2024-05-18 06:54:14 +0700

    add post mock up api for topics

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const app = express()
 
-let notes = []
+let topics = []
+
+app.use(express.json())
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
+app.get('/api/topics', (request, response) => {
+  response.json(topics)
+})
+
+app.post('/api/topics', (request, response) => {
+  console.log(request.body)
+  const topic = request.body
+  topics = topics.concat(topic)
+  console.log(topic)
+  console.log(topics)
+  response.json(topic)
 })
 
 const PORT = 3001
