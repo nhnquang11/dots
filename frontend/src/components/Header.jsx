0982e02a9544d4d66946e2855efe751d7bdf84da
Nguyen Hoang Nhat Quang <112571466+nhnquang11@users.noreply.github.com>
2024-05-17 13:51:20 +0700

    add profile dropdown

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react"
+
 const Header = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+
+  const handleDropdownOnClick = () => {
+    setIsDropdownOpen(!isDropdownOpen)
+  }
+
   return (
     <nav>
       <div className="flex gap-5 justify-around my-3 px-5">
         {/* Logo */}
         <div>
-          <a href="#" className="font-serif text-xl font-bold">Dots</a>
+          <a href="#" className="font-serif text-xl font-bold text-gray-950">Dots</a>
         </div>
 
         {/* Search bar */}
@@ -15,11 +23,27 @@ const Header = () => {
             placeholder="Search" />
         </div>
 
-        {/* Profile */}
-        <div className="flex justify-center align-center">
-          <button type="button">
-            <img className="h-8 w-8 rounded-full object-cover" src="https://huggingface.co/KappaNeuro/director-darren-aronofsky-style/resolve/main/2345259.jpeg" />
-          </button>
+        {/* Profile dropdown */}
+        <div className="relative">
+          <div className="flex justify-center align-center">
+            <button type="button" onClick={handleDropdownOnClick}>
+              <img className="h-8 w-8 rounded-full object-cover" src="https://huggingface.co/KappaNeuro/director-darren-aronofsky-style/resolve/main/2345259.jpeg" />
+            </button>
+          </div>
+
+          {
+            isDropdownOpen &&
+            <div className="flex flex-col absolute w-48 bg-gray-50 shadow-sm text-sm rounded border right-0 top-9 font-serif font-extralight text-gray-500">
+              <div className="flex flex-col py-4 gap-4">
+                <a href="" className="px-6 hover:text-gray-950">Profile</a>
+                <a href="" className="px-6 hover:text-gray-950">Dashboard</a>
+              </div>
+              <hr className="" />
+              <div className="flex flex-col py-4">
+                <a href="" className="px-6 hover:text-gray-950">Sign Out</a>
+              </div>
+            </div>
+          }
         </div>
       </div>
     </nav>
