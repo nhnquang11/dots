06b2fc38ae3ec7f53e18923af19db59d5bd68c0a
Nguyen Hoang Nhat Quang <quangnhn33@gmail.com>
2024-05-21 18:16:38 +0700

    add users table

--- frontend/src/components/Comments.jsx
+++ frontend/src/components/Comments.jsx
@@ -1,6 +1,8 @@
 const Comments = () => {
   return (
-    <div>Comments</div>
+    <div className="mb-20 px-3 mx-auto max-w-screen-xl">
+      <h3 className="mt-16 font-serif text-neutral-900 font-semibold text-4xl text-center px-2">Comments</h3>
+    </div>
   )
 }
 
--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,6 +1,81 @@
+import { useEffect, useState } from 'react'
+import userSevice from '../services/userService'
+import { newestToOldest } from '../utils'
+import { dateFormat } from '../utils'
+import crossIcon from "../assets/cross.png"
+import checkIcon from "../assets/check.png"
+
 const Users = () => {
+  const [numToShow, setNumToShow] = useState(2)
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    userSevice.getAll().then((data) => {
+      console.log(data)
+      setUsers(data.sort(newestToOldest))
+    })
+  }, [])
+
+  const loadMoreUsers = () => {
+    setNumToShow(numToShow + 5)
+  }
+
+  const deleteUser = (id) => {
+    // userSevice.remove(id).then(() => {
+    //   setUsers(users.filter(user => user.id !== id))
+    // })
+    console.log('Delete user with id:', id)
+  }
+
   return (
-    <div>Users</div>
+    <div className="mb-20 px-3 mx-auto max-w-screen-xl">
+      <h3 className="mt-16 font-serif text-neutral-900 font-semibold text-4xl text-center px-2">Users</h3>
+
+      <div className="mt-10 font-serif flex items-center justify-center w-full overflow-x-auto border rounded">
+        <table className="w-full text-sm text-left text-neutral-500 table-auto">
+          <thead className="text-xs text-neutral-700 uppercase bg-neutral-100">
+            <tr>
+              <th scope="col" className="py-3 px-4 sm:py-4 sm:px-6">#</th>
+              <th scope="col" className="py-3 px-4 sm:py-4 sm:px-6">Created</th>
+              <th scope="col" className="py-3 px-4 sm:py-4 sm:px-6">Avatar</th>
+              <th scope="col" className="py-3 px-4 sm:py-4 sm:px-6">Username</th>
+              <th scope="col" className="py-3 px-4 sm:py-4 sm:px-6">Email</th>
+              <th scope="col" className="py-3 px-4 sm:py-4 sm:px-6">Admin</th>
+              <th scope="col" className="py-3 px-4 sm:py-4 sm:px-6">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users.slice(0, numToShow).map((user, index) => (
+                <tr key={user.id} className="border-t bg-neutral-50">
+                  <td className="py-3 px-4 sm:py-4 sm:px-6">{index + 1}</td>
+                  <td className="py-3 px-4 sm:py-4 sm:px-6">{dateFormat(user.registrationDate)}</td>
+                  <td className="py-3 px-4 sm:py-4 sm:px-6">
+                    <img className='w-10 h-10 object-cover rounded-full' src={user.profilePic} alt="" />
+                  </td>
+                  <td className="py-3 px-4 sm:py-4 sm:px-6">{user.username}</td>
+                  <td className="py-3 px-4 sm:py-4 sm:px-6">{user.email}</td>
+                  <td className="py-3 px-4 sm:py-4 sm:px-6">
+                    <img className='w-5 h-5' src={user.isAdmin ? checkIcon : crossIcon} alt="" />
+                  </td>
+                  <td className="py-3 px-4 sm:py-4 sm:px-6">
+                    <button className="border border-red-500 rounded px-2 py-1 text-xs text-red-500 hover:bg-red-500 hover:text-neutral-50" onClick={() => deleteUser(user.id)}>Delete</button>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+      {
+        numToShow < users.length && (
+          <div className="mt-3 flex justify-center">
+            <button onClick={loadMoreUsers} className="leading-8 font-extralight text-sm font-serif px-3 rounded bg-neutral-900 text-white hover:bg-neutral-600 transition duration-100 ease-in-out">
+              Load more
+            </button>
+          </div>
+        )
+      }
+    </div>
   )
 }
 
